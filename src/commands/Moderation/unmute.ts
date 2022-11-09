2c4ec650e0990864c4898c7c20260dd6d7cf248d
Fawnino <inocayari@gmail.com>
2022-11-09 18:00:42 +0800

    Not so big changes

--- src/commands/Moderation/unmute.ts
+++ src/commands/Moderation/unmute.ts
@@ -24,6 +24,73 @@ export default new CelestineCommand({
 			type: ApplicationCommandOptionType.String,
 		},
 	],
+	async messageRun(message, args) {
+		const targetUser =
+			message.mentions.members?.first() ||
+			message.guild?.members.cache.get(args[0]);
+
+		const unmuteReason = args.slice(1).join(" ") || "No reason provided.";
+
+		if (
+			!(message.member as GuildMember).permissions.has([
+				PermissionsBitField.Flags.ManageMessages,
+			])
+		)
+			return message.reply({
+				content:
+					"You do not have the sufficient permission `ManageMessages` to use this command!",
+			});
+
+		if (!targetUser)
+			return message.reply({ content: "Please specify a user to unmute!" });
+
+		const targetMember = message.guild?.members.cache.get(
+			targetUser.id,
+		) as GuildMember;
+
+		if (targetMember.id === message.author.id)
+			return message.reply({
+				content: `You can't unmute yourself lmao.`,
+			});
+
+		if (!targetMember.isCommunicationDisabled())
+			return message.reply({
+				content: "The user you're trying to mute is not muted!",
+			});
+
+		if (!targetMember.manageable || !targetMember.moderatable)
+			return message.reply({
+				content:
+					"The user that you are trying to unmute is higher than I am in the role hierarchy, meaning that I cannot moderate them.",
+			});
+		if (
+			targetMember.roles.highest.position >
+			(message.member! as GuildMember).roles.highest.position
+		)
+			return message.reply({
+				content:
+					"The user that you are trying to unmute is higher than you are in the role hierarchy.",
+			});
+
+		const successUnmute = new EmbedBuilder()
+			.setTitle("Success!")
+			.setColor("Green")
+			.setDescription(
+				`**Successfully unmuted:** ${targetMember}\n**Unmute reason:** ${unmuteReason}`,
+			)
+			.setFooter({
+				text: `${message.guild?.name}`,
+				iconURL: message.guild?.iconURL({ forceStatic: true }) as string,
+			})
+			.setThumbnail(`${targetMember.displayAvatarURL({ forceStatic: true })}`)
+			.setTimestamp();
+
+		await targetMember.disableCommunicationUntil(null, unmuteReason);
+
+		return message.reply({
+			embeds: [successUnmute],
+		});
+	},
 	async commandRun(interaction) {
 		const targetUser = interaction.options.getUser("user", true);
 		const unmuteReason =
