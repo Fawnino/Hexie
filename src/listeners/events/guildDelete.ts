73379fdb5f1b3c9cb1be3c4532749ffdab0bb987
Fawnino <inocayari@gmail.com>
2022-11-12 19:17:18 +0800

    Not so big changes

--- src/listeners/events/guildDelete.ts
+++ src/listeners/events/guildDelete.ts
@@ -1,11 +1,13 @@
-import { Listener } from "#lib/structures";
+import { Listener, Logger } from "#lib/structures";
 import { redBright } from "colorette";
+
+const logger = new Logger();
 
 export default new Listener({
 	event: "guildDelete",
 	async run(guild) {
-		console.log(
-			redBright(`Removed from a guild: ${guild.name} // ${guild.id}`),
+		logger.info(
+			redBright(`Removed from a Guild: ${guild.name} // ${guild.id}`),
 		);
 		// Logs a bright red text when the bot is kicked out of a guild.
 	},
